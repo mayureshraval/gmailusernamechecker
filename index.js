4affd555afb0c024bbbf696df51d6add1c5003da
mayureshraval <mayureshraval2012@gmail.com>
2024-09-09 22:23:25 +0530

    Created index.ejs and form to submit using post and created handler in express page for it.

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req,res)=>{
-    res.send('App running!');
+    // res.send('App running!');
+    res.render('index.ejs')
+})
+app.post('/', (req,res)=>{
+    // res.send('App running!');
+
+    let username = req.body.username;
+    res.render('index.ejs', {username: username});
 })
 
 app.listen(port, ()=>{
